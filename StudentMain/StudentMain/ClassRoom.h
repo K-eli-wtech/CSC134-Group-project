8ed08e95bfeca6a03d1cfc49b020276183dd6b67
jayates04 <95187218+jayates04@users.noreply.github.com>
2021-12-03 17:08:26 -0500

    Update ClassRoom.h

--- StudentMain/StudentMain/ClassRoom.h
+++ StudentMain/StudentMain/ClassRoom.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class ClassRoom
 {
+public:
+	ClassRoom(); //Constructor
+	~ClassRoom(); //Destructor
+	void readDataFromFile(string filename);
+	void sortOnAverage();
+	void sortOnLastName();
+	void print();
+	double getClassAverage();
+	int getStudentCount();
+private:
+	string name;
+	int numStudents;
+	student* studentArray; //replace student with name for student object
 };
 
