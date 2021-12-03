433b6825f6554d471ac02bafea0aee5a46718608
Caleb <37146734+cmtutor123@users.noreply.github.com>
2021-12-03 14:07:24 -0500

    Update StudentMain.cpp

--- StudentMain/StudentMain/StudentMain.cpp
+++ StudentMain/StudentMain/StudentMain.cpp
@@ -1,20 +1,38 @@
-// StudentMain.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "ClassRoom.h"
+#include "Student.h"
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Create a ClassRoom Object
+    ClassRoom classRoomObject;
+
+    // Use the ClassRoom Object to read the Student Objects from the input file "students.txt"
+    string fileName = "students.txt";
+    classRoomObject.readDataFromFile(fileName);
+
+    // Use the ClassRoom Object to sort list of students by student average
+    classRoomObject.sortOnAverage();
+
+    // Display the list of students by student average
+    cout << "Students by Student Average" << endl;
+    classRoomObject.print();
+
+    // Use the ClassRoom Object to sort list of students by student last name
+    classRoomObject.sortOnLastName();
+
+    // Display the list of students by student last name
+    cout << "Students by Student Last Name" << end;
+    classRoomObject.print();
+
+    // Use the ClassRoom Object to display the average grade of all students
+    double classAverage = classRoomObject.getClassAverage();
+    cout << "Class Average: " << classAverage << endl;
+
+    // Use the ClassRoom Object to display the number of Student objects created
+    int studentCount = classRoomObject.getStudentCount();
+    cout << "Student Count: " << studentCount << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
