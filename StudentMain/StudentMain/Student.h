620933f3608edd95e338bd0db860f9b4ae8db6c9
Caleb <37146734+cmtutor123@users.noreply.github.com>
2021-12-03 15:30:58 -0500

    Update Student.h

--- StudentMain/StudentMain/Student.h
+++ StudentMain/StudentMain/Student.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <string>
+
 class Student
 {
+private:
+	string FirstName;
+	string LastName;
+	string SocialSecurityNumber;
+	double ExamGrades[4];
+	double AverageExamGrade;
+	static int StudentNumber;
+public:
+	string getFirstName();
+	void setFirstName(string firstName);
+	string getLastName();
+	void setLastName(string lastName);
+	string getSocialSecurityNumber();
+	void setSocialSecurityNumber(string socialSecurityNumber);
+	double getExamGrade(int index);
+	double setExamGrade(double examGrade, int index);
+	void setExamGrades(double firstGrade, double secondGrade, double thirdGrade, double fourthGrade);
+	double getAverageExamGrade();
+	void setAverageExamGrade(double averageExamGrade);
+	int getStudentNumber();
+	double calculateAverageExamGrade();
+	void print();
 };
-
