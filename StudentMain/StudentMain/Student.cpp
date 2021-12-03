88785123b0d3b4bcb7be8f0188f9951fcca0fc44
Caleb <37146734+cmtutor123@users.noreply.github.com>
2021-12-03 16:57:35 -0500

    Update Student.cpp

--- StudentMain/StudentMain/Student.cpp
+++ StudentMain/StudentMain/Student.cpp
@@ -1,1 +1,85 @@
 #include "Student.h"
+#include <iostream>
+
+using namespace std;
+
+string Student::getFirstName()
+{
+	return FirstName;
+}
+
+void Student::setFirstName(string firstName)
+{
+	FirstName = firstName;
+}
+
+string Student::getLastName()
+{
+	return LastName;
+}
+
+void Student::setLastName(string lastName)
+{
+	LastName = lastName;
+}
+
+string Student::getSocialSecurityNumber()
+{
+	return SocialSecurityNumber;
+}
+
+void Student::setSocialSecurityNumber(string socialSecurityNumber)
+{
+	SocialSecurityNumber = socialSecurityNumber;
+}
+
+double Student::getExamGrade(int index)
+{
+	return ExamGrades[index];
+}
+
+void Student::setExamGrade(double examGrade, int index)
+{
+	ExamGrades[index] = examGrade;
+}
+
+void Student::setExamGrades(double firstGrade, double secondGrade, double thirdGrade, double fourthGrade)
+{
+	ExamGrades[0] = firstGrade;
+	ExamGrades[1] = secondGrade;
+	ExamGrades[2] = thirdGrade;
+	ExamGrades[3] = fourthGrade;
+}
+
+double Student::getAverageExamGrade()
+{
+	return AverageExamGrade;
+}
+
+void Student::setAverageExamGrade(double averageExamGrade)
+{
+	AverageExamGrade = averageExamGrade;
+}
+
+int Student::getStudentNumber()
+{
+	return StudentNumber;
+}
+
+double Student::calculateAverageExamGrade()
+{
+	double gradeSum = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		gradeSum += getExamGrade(i);
+	}
+	return gradeSum / 4;
+}
+
+void Student::print()
+{
+	cout << "Student Name: " << getFirstName() << ' ' << getLastName() << endl;
+	cout << "Social Security Number: " << getSocialSecurityNumber() << endl;
+	cout << "Exam Grades: " << getExamGrade(0) << ", " << getExamGrade(1) << ", " << getExamGrade(2) << ", " << getExamGrade(3) << endl;
+	cout << "Average Exam Grade: " << getAverageExamGrade() << endl;
+}
