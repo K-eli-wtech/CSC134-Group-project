8fff22fd1e7e9c75784644a9d6e49039976743ad
Caleb <37146734+cmtutor123@users.noreply.github.com>
2021-12-04 07:05:17 -0500

    Update ClassRoom.h

--- StudentMain/StudentMain/ClassRoom.h
+++ StudentMain/StudentMain/ClassRoom.h
@@ -1,22 +1,58 @@
+
+//////////
+//
+// Filename: ClassRoom.h
+// Date: December 4, 2021
+// Programmers: Jacob Yates, Caleb Tutor
+// 
+// Description: interface for the ClassRoom class
+//
+//////////
+
 #pragma once
 #include <string>
 #include "Student.h"
 
 using namespace std;
 
+//////////
+//
+// Class: ClassRoom
+// 
+// Description: Contains information about up to 24 Students, can sort and display that information
+// 
+// Data Members:
+//  StudentCount
+//  Name
+//  StudentArray
+// 
+// Member Functions:
+//  ClassRoom
+//  ~ClassRoom
+//  getClassAverage
+//  getStudentCount
+//  incrementStudentCount
+//  readDataFromFile
+//  sortOnAverage
+//  sortOnLastName
+//  print
+//
+//////////
+
 class ClassRoom
 {
 public:
 	ClassRoom();		//Constructor
 	~ClassRoom();		//Destructor
+	double getClassAverage();		//Gets total class average
+	int getStudentCount();		//Grabs total count of students
+	void incrementStudentCount();
 	void readDataFromFile(string filename);		//Reads file data
 	void sortOnAverage();		//Sorts student ar by average grade
 	void sortOnLastName();		//Sorts student array by C-String value of last name
 	void print();		//Prints data
-	double getClassAverage();		//Gets total class average
-	int getStudentCount();		//Grabs total count of students
 private:
-	string name;
-	Student *studentArray; 
-	static int numStudents;
+	static int StudentCount;
+	string Name;
+	Student *StudentArray;
 };
