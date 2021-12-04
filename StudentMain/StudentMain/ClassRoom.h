336809a77417ad3fa98a5a2f2d61195e91406d30
Caleb <37146734+cmtutor123@users.noreply.github.com>
2021-12-04 09:29:46 -0500

    Update ClassRoom.h

--- StudentMain/StudentMain/ClassRoom.h
+++ StudentMain/StudentMain/ClassRoom.h
@@ -42,17 +42,62 @@ using namespace std;
 class ClassRoom
 {
 public:
-	ClassRoom();		//Constructor
-	~ClassRoom();		//Destructor
-	double getClassAverage();		//Gets total class average
-	int getStudentCount();		//Grabs total count of students
+	ClassRoom();
+	// Default Constructor
+	// Initializes the classroom name and array of Students
+	// Postcondition:
+	//  Name = "CSC-134"
+	//  StudentArray = new Student[24]
+	
+	~ClassRoom();
+	// Deconstructor
+	// deletes StudentArray
+	// Postcondition:
+	//  delete[] StudentArray
+	
+	double getClassAverage();
+	// Function that returns the total class average
+	// Postcondition:
+	//  returns the sum of all Student.AverageExamGrade in StudentArray divided by StudentCount
+	
+	int getStudentCount();
+	// Function that returns the total number of students
+	// Postcondition:
+	//  returns StudentCount
+	
 	void incrementStudentCount();
-	void readDataFromFile(string filename);		//Reads file data
-	void sortOnAverage();		//Sorts student ar by average grade
-	void sortOnLastName();		//Sorts student array by C-String value of last name
-	void print();		//Prints data
+	// Function that increments StudentCount
+	// Postcondition:
+	//  StudentCount += 1
+	
+	void readDataFromFile(string filename);
+	// Function that stores the data from the file filename into Student objects in StudentArray
+	// Postcondition:
+	//  StudentArray gains Student objects per line of filename, up to a maximum of 24
+	
+	void sortOnAverage();
+	// Function that sorts the students in StudentArray by their average grade
+	// Postcondition:
+	//  StudentArray becomes sorted based on the values of Student.AverageExamGrade
+	
+	void sortOnLastName();
+	// Function that sorts the students in StudentArray by their last name
+	// Postcondition:
+	//  StudentArray becomes sorted based on the values of Student.LastName
+	
+	void print();
+	// Function that prints the information about the students using Student.print()
+	// Postcondition:
+	//  for each Student in StudentArray
+	//  Student.print()
+	
 private:
 	static int StudentCount;
+	// static variable that stores the number of students
+
 	string Name;
+	// variable that stores the name of the classroom
+
 	Student *StudentArray;
+	// array that stores pointers to Student objects
 };
