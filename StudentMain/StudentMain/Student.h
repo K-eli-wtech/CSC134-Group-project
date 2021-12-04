032f114ac2400935d0c942c411ae5bd9199fb9d5
Caleb <37146734+cmtutor123@users.noreply.github.com>
2021-12-03 19:01:27 -0500

    Update Student.h

--- StudentMain/StudentMain/Student.h
+++ StudentMain/StudentMain/Student.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <string>
 
+using namespace std;
+
 class Student
 {
 public:
-	std::string getFirstName();
-	void setFirstName(std::string firstName);
-	std::string getLastName();
-	void setLastName(std::string lastName);
-	std::string getSocialSecurityNumber();
-	void setSocialSecurityNumber(std::string socialSecurityNumber);
+	static int StudentNumber;
+	string getFirstName();
+	string getLastName();
+	string getSocialSecurityNumber();
+	void setSocialSecurityNumber(string socialSecurityNumber);
 	double getExamGrade(int index);
 	void setExamGrade(double examGrade, int index);
 	void setExamGrades(double firstGrade, double secondGrade, double thirdGrade, double fourthGrade);
@@ -19,11 +20,11 @@ public:
 	double calculateAverageExamGrade();
 	void print();
 	Student();
+	Student(string firstName, string lastName, string socialSecurityNumber, double firstGrade, double secondGrade, double thirdGrade, double fourthGrade);
 private:
-	std::string FirstName;
-	std::string LastName;
-	std::string SocialSecurityNumber;
+	const string FirstName;
+	const string LastName;
+	string SocialSecurityNumber;
 	double ExamGrades[4];
 	double AverageExamGrade;
-	int StudentNumber;
 };
